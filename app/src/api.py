7b4008ff2c0509c16a0afc39e79c27a6d5b099b6
aprds <andipratama.pe@gmail.com>
2022-07-13 12:00:50 +0700

    Deployment

---
+++ app/src/api.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import joblib
+import yaml
+from preprocessing import preprocess
+from feature_engineering import feature_eng
+from utils import read_yaml
+from prediction import main_predict
+from fastapi import FastAPI, Form, Header, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from tqdm import tqdm
+
+tqdm.pandas()
+
+PREPROCESSING_CONFIG_PATH = "../config/preprocessing_config.yaml"
+FEATURE_ENGINEERING_CONFIG_PATH = "../config/feature_engineering_config.yaml"
+
+model = joblib.load('../model/fitted_model.pkl')
+
+param_feat_eng = read_yaml(FEATURE_ENGINEERING_CONFIG_PATH)
+param_preprocess = read_yaml(PREPROCESSING_CONFIG_PATH)
+
+app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def res_constructor(predict, proba):
+    res = {'result': predict, 'proba': proba}
+    return res
+
+@app.post("/predict/")
+
+def predict_api(fluid = Form(), operator=Form(), project_status=Form(), inplace=Form(), depth=Form(), temp= Form(), poro=Form(), perm=Form(), saturate=Form(), api_dens=Form(), visc=Form(), avg_fluid_rate=Form(), location=Form(), region=Form()):
+    
+    df = pd.DataFrame((fluid,operator,project_status,inplace,depth,temp,poro,perm,saturate,api_dens,visc,avg_fluid_rate,location,region), 
+                        columns=[fluid,operator,project_status,inplace,depth,temp,poro,perm,saturate,api_dens,visc,avg_fluid_rate,location,region] 
+                        )
+    try:
+        predict, proba = main_predict(df, model, param_preprocess, param_feat_eng)
+        res = res_constructor(predict, proba)
+        return res
+
+    except Exception as e:
+        return {'result': "", 'proba': "", 'message': str(e)}
+    
